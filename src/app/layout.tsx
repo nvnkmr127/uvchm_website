514264bdfdd041efd7b462c7e8820be9ababcc4a
Naveen <nvnkmr127@gmail.com>
2026-08-25 16:14:36 +0530

    feat: add custom favicon to site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,9 @@ export const metadata: Metadata = {
   verification: {
     google: 'YOUR_GOOGLE_SEARCH_CONSOLE_VERIFICATION_CODE',
   },
+  icons: {
+    icon: '/fav.png',
+  },
 };
 
 const jsonLd = {
